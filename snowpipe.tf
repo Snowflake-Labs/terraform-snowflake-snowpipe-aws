5a31a1a9ff747ec1fea6185b000078e718b67f37
sfc-gh-kmaurya <98528579+sfc-gh-kmaurya@users.noreply.github.com>
2023-04-25 12:39:54 +0530

    updating the copy statement so that it works even if target_column key-pair is not present or is empty

--- snowpipe.tf
+++ snowpipe.tf
@@ -1,9 +1,13 @@
 locals {
   copy_statement = <<-EOF
 %{if length(var.custom_ingest_columns) > 0}
-COPY INTO ${var.database_name}.${var.schema_name}.${var.destination_table_name}(
-    ${join(", ", var.custom_ingest_columns["target_columns"])}
-)
+  %{if length(var.custom_ingest_columns) == 1 || var.custom_ingest_columns["target_columns"] == []}
+    COPY INTO ${var.database_name}.${var.schema_name}.${var.destination_table_name}
+  %{else}
+    COPY INTO ${var.database_name}.${var.schema_name}.${var.destination_table_name}(
+        ${join(", ", var.custom_ingest_columns["target_columns"])}
+    )
+  %{endif}
 FROM (
     SELECT ${join(", ", var.custom_ingest_columns["source_columns"])}
     FROM @${var.database_name}.${var.schema_name}.${var.stage_name}
